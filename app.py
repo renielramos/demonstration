99d82a92d627d202e72bad3bf867f81d3b499872
arjohn <arjohnverastigue@gmail.com>
2025-04-11 19:51:59 +0800

    update

--- app.py
+++ app.py
@@ -4,13 +4,3 @@ def celsius_to_fahrenheit(c):
 
 def fahrenheit_to_celsius(f):
     return round((f - 32) * 5/9, 2)
-
-
-if __name__ == "__main__":
-    c = 25
-    f = celsius_to_fahrenheit(c)
-    print(f"{c}°C is {f}°F")
-
-    f = 77
-    c = fahrenheit_to_celsius(f)
-    print(f"{f}°F is {c}°C")
